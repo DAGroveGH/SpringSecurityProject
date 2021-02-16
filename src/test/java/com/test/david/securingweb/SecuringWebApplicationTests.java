f529e205652fb89a9874e4d66771a84b97b0f281
DAGroveGH <dagrove7@gmail.com>
2021-02-16 16:37:45 -0500

    Initial commit

---
+++ src/test/java/com/test/david/securingweb/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.david.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
